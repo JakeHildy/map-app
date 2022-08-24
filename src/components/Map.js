d445a0f052707969784aec5dce69a382ac828f26
Jacob Hildebrandt <jacob@commerce7.com>
2022-08-24 10:57:02 -0700

    Show markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,39 +1,69 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, Fragment } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 import axios from "axios";
 
 const Map = () => {
-  const [customers, setCustomers] = useState(null);
-  const [total, setTotal] = useState(null);
+  const [customerAddresses, setCustomers] = useState(null);
+  const [markerLocations, setMarkerLocations] = useState(null);
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
 
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+  const center = useMemo(() => ({ lat: 41.5, lng: -99 }), []);
 
   const getCustomerAddresses = async () => {
-    const response = await axios.get(`https://api.commerce7.com/v1/customer`, {
-      headers: {
-        Authorization: `Basic ${process.env.REACT_APP_API_AUTH}`,
-        tenant: "jacob-sandbox-account",
-      },
+    const response = await axios.get(
+      `https://api.commerce7.com/v1/customer-address`,
+      {
+        headers: {
+          Authorization: `Basic ${process.env.REACT_APP_API_AUTH}`,
+          tenant: "jacob-sandbox-account",
+        },
+      }
+    );
+    setCustomers(response.data.customerAddresses);
+  };
+
+  const getMarkerLocations = async (customerAddresses) => {
+    const requests = customerAddresses.map((customerAddress) => {
+      const { address, city, stateCode } = customerAddress;
+      const addressString = `${address} ${city} ${stateCode}`;
+      return axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+          addressString
+        )}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+      );
     });
-    setCustomers(response.data.customers);
-    setTotal(response.data.total);
+    const locationsRaw = await Promise.all(requests);
+    // console.log(locationsRaw);
+    const locations = locationsRaw.map(
+      (location) => location.data.results[0]?.geometry.location
+    );
+    setMarkerLocations(locations);
   };
 
   useEffect(() => {
     getCustomerAddresses();
   }, []);
 
-  if (!customers || !isLoaded) return null;
+  useEffect(() => {
+    if (customerAddresses && !markerLocations) {
+      getMarkerLocations(customerAddresses);
+    }
+  }, [customerAddresses, markerLocations]);
 
-  console.log(customers);
+  if (!markerLocations || !isLoaded) return null;
+
+  console.log(markerLocations);
 
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={{ lat: 44, lng: -80 }} />
+    <GoogleMap zoom={4} center={center} mapContainerClassName="map-container">
+      {markerLocations.map((location, index) => (
+        <Fragment key={`${index}-${Math.random()}`}>
+          {!!location && <Marker position={location} />}
+        </Fragment>
+      ))}
     </GoogleMap>
   );
 };
