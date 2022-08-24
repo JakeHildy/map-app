7eaddc332bc044f5b1d29c1422df58b8c77eb688
Jacob Hildebrandt <jacob@commerce7.com>
2022-08-23 19:01:54 -0700

    Use Google Maps

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Map from "./components/Map";
 const App = () => {
   return (
     <div className="App">
-      <h1>Map App</h1>
+      <h2>
+        Welcome to Jacob's map app. Take a look at all your customers below!
+      </h2>
       <Map />
     </div>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
 import axios from "axios";
 
 const Map = () => {
   const [customers, setCustomers] = useState(null);
   const [total, setTotal] = useState(null);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
 
   const getCustomerAddresses = async () => {
     const response = await axios.get(`https://api.commerce7.com/v1/customer`, {
@@ -20,11 +27,15 @@ const Map = () => {
     getCustomerAddresses();
   }, []);
 
-  if (!customers) return null;
+  if (!customers || !isLoaded) return null;
 
   console.log(customers);
 
-  return <div>Map</div>;
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={{ lat: 44, lng: -80 }} />
+    </GoogleMap>
+  );
 };
 
 export default Map;
