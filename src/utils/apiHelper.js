97b5e718aba100bb61c08fe0a0e5fa136d8a5178
Jacob Hildebrandt <jacob@commerce7.com>
2022-09-09 09:18:21 -0700

    Add Buffer package

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -1,5 +1,7 @@
+import { Buffer } from "buffer";
+
 export const getAuthorization = () => {
   const authorizationString = `${process.env.REACT_APP_TENANT_ID}:${process.env.REACT_APP_SECRET_KEY}`;
-  const authKey = Buffer.from(authorizationString).toString("base64");
+  const authKey = Buffer.from(authorizationString, "base64");
   return authKey;
 };
